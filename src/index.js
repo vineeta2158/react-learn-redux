ab30b017aad769ebc7aedac748077875d7660908
vineeta kshatriya <vineeta.kshatriya@viit.ac.in>
2021-04-20 23:05:57 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import allReducer from './reducers';
+
+const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 
 ReactDOM.render(
   <React.StrictMode>
